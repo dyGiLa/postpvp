2602659420f06e33c711f347e5a7c20257f2f31e
timohyva <hyvarinentimo00@gmail.com>
2025-09-01 01:55:18 +0300

    add pvbatch script

---
+++ A-phase-A-matrix-vectorizing/pvbatch-A-phase-matirx-vector-decomposition.py
@@ -0,0 +1,33 @@
+# trace generated using paraview version 5.13.3
+#import paraview
+#paraview.compatibility.major = 5
+#paraview.compatibility.minor = 13
+
+#### import the simple module from the paraview
+from paraview.simple import *
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+# load plugin
+LoadPlugin('./PyAlg-mnl-vector-filter.py', remote=False, ns=globals())
+
+# create a new 'XML Partitioned Unstructured Grid Reader'
+piopvti = XMLPartitionedUnstructuredGridReader(registrationName='pio.pvtu', FileName=['./pio.pvti'])
+piopvti.PointArrayStatus = ['gapA', 'u11', 'u12', 'u13', 'u21', 'u22', 'u23', 'u31', 'u32', 'u33',
+                            'v11', 'v12', 'v13', 'v21', 'v22', 'v23', 'v31', 'v32', 'v33']
+
+# Properties modified on piopvti
+piopvti.TimeArray = 'None'
+
+UpdatePipeline(time=0.0, proxy=piopvti)
+
+# create a new 'DMNL-A-phase-A-matrix Filter'
+dMNLAphaseAmatrixFilter1 = DMNLAphaseAmatrixFilter(registrationName='DMNLAphaseAmatrixFilter1', Input=piopvti)
+
+UpdatePipeline(time=0.0, proxy=dMNLAphaseAmatrixFilter1)
+
+# save data
+SaveData('./dmnl/dmnl-phi-A-phase-AMatrix.pvtu',
+         proxy=dMNLAphaseAmatrixFilter1,
+         ChooseArraysToWrite=1,
+         PointDataArrays=['U1_phi', 'd1', 'd2', 'd3', 'l1', 'l2', 'l3', 'm1', 'm2', 'm3', 'n1', 'n2', 'n3'])
